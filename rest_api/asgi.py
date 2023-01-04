9fb64fb4b44689e484d5a7f87a7fe00a050548ea
matsu <donguri47@gmail.com>
2023-01-04 09:56:31 +0900

    setting add

--- rest_api/asgi.py
+++ rest_api/asgi.py
@@ -1,8 +1,8 @@
+
+
 """
 ASGI config for rest_api project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
